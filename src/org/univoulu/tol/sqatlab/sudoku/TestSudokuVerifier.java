6be9017308777bf99ffd3fcb402458a5a5ec0a87
somename <someemail>
2015-09-30 16:10:39 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -20,7 +20,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void checkCharactersIncludingMinus() {
 		
-		String candidateSolution = "9134ss";
+		String candidateSolution = "9134";
 		
 		assertEquals(-1, SudokuVerifier.charactersPositive(candidateSolution));
 	}
